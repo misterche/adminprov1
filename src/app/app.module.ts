add29db22d7534b9b48d836aa0be297853d3f559
Guillermo Moran <morang1302@gmail.com>
2018-02-06 17:25:59 -0500

    limpieza en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//referencia a las rutas de las paginas
 import { APP_ROUTES} from './app.routes'
 
+//referencia a los modulos
+import {PagesModule} from './pages/pages.module'
+
+//componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraficasComponent } from './pages/graficas/graficas.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SiderbarComponent } from './shared/siderbar/siderbar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    GraficasComponent,
-    HeaderComponent,
-    SiderbarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
